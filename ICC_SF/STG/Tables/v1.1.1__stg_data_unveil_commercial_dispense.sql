b0898400ae1c375ee24a329b5ebb6c3d640156df
akibkhan15 <sultanakib12@gmail.com>
2024-04-04 17:44:10 +0530

    commiiting demo

--- ICC_SF/STG/Tables/v1.1.1__stg_data_unveil_commercial_dispense.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Car (
-    car_id INTEGER AUTOINCREMENT PRIMARY KEY,
-    make VARCHAR(50),
-    model VARCHAR(50),
-    year INTEGER,
-    color VARCHAR(20),
-    price DECIMAL(10, 2)
-);
